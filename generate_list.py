297a2e544caf0364eb96b38c2e92fd560045cb7d
glubsy <glubsy@users.noreply.github.com>
2022-02-18 14:36:31 +0100

    Add generate id list from playboard
    * Add script to se the playboard.co API to fetch
    videoIds and generate the list directly.

---
+++ generate_list.py
@@ -0,0 +1,124 @@
+#!/bin/env python3
+#
+# To create a list of Youtube video IDs from playboard.co by copying the root html element, then use:
+# grep --color=never -rioP 'href="/en/video/(.*){11}"' computed_page.html | sed -n -r 's#.*href="/en/video/([^"]*).*#\1#p' > video_ids_reversed.txt
+# tac video_ids_reversed.txt > video_ids.txt
+# TODO Fetch videos from Youtube and compare with what playboard returns
+
+from sys import argv
+from typing import List, Tuple
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote
+import json
+import gzip
+from io import BytesIO
+import logging
+log = logging.getLogger(__name__)
+logging.basicConfig()
+# log.setLevel(logging.DEBUG)
+
+BASE_URL = "https://api.playboard.co/v1/"
+PATH = "search"
+TYPE = "video"
+ENDPOINT = f"{BASE_URL}{PATH}/{TYPE}"
+
+HEADERS = {
+  "origin": "https://playboard.co",
+  "referer": "https://playboard.co/",
+  "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
+  "accept" "application/json, text/plain, */*",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en,en-US",
+  "content-type": "application/json"
+}
+
+
+def req(url, params, headers=None, method="POST", data=None):
+  """Just an alternative method with the Requests module."""
+  import requests
+  headers = HEADERS if headers is None else headers
+  # req = requests.Request(method=method, url=url, headers=headers)
+  res = requests.post(url, data=None, headers=headers, params=params)
+  return res.json()
+
+
+def do_request(url, headers=None, method="POST", data=None):
+  headers = HEADERS if headers is None else headers
+  req = Request(
+    url,
+    headers=HEADERS,
+    data=data,
+    method=method
+  )
+  log.debug(f"req: {req.full_url} {req.headers}")
+  with urlopen(req) as res:
+    if res.headers.get("content-encoding") == "gzip":
+      content = gzip.decompress(res.read())
+      return json.load(BytesIO(content))
+
+    content = str(res.read().decode())
+    log.debug(f"data: {content}")
+    return json.loads(content)
+
+
+def get_videos_for_channel(channel_id: str) -> List[Tuple[str, int]]:
+  # Each tuple returned has (videoId, status)
+  vids = []
+  total = 0
+  has_next = True
+  cursor = None
+  params = {
+    "channelId": channel_id,
+    "sortTypeId": "10",
+  }
+  print("Found videoIds:")
+
+  while has_next:
+    if cursor is not None:
+      params.update({"cursor": cursor})
+
+    _json = do_request(url=ENDPOINT + "?" + urlencode(params, quote_via=quote))
+    # _json = req(url=ENDPOINT, params=urlencode(params, quote_via=quote))
+
+    has_next = _json.get("hasNext")
+    cursor = _json.get("cursor")
+    total = _json.get("total")
+    for vid in _json.get("list"):
+      print(
+        f"{vid['videoId']}"
+        + (' (deleted)' if vid.get('status') == 2 else "")
+      )
+      vids.append(
+        (vid["videoId"], vid.get("status"))
+      )
+
+  if total != len(vids):
+    print(f"WARNING: playboard advertised {total} videos but only returned {len(vids)}.")
+
+  return vids
+
+
+def main():
+  channel_id = argv[1]
+  videos = get_videos_for_channel(channel_id)
+
+  # "2" seems to indicate removed?, "1" and "3" appear to be the same?
+  possibly_removed = [v for v,s in videos if s == 2]
+
+  print(f"Found {len(videos)} IDs, among which {len(possibly_removed)} "
+        "are advertised as being deleted.")
+
+  videos_file = f"playboard_ids_{channel_id}.txt"
+  with open(videos_file, "w") as f:
+    for _id, _ in videos:
+      f.write(_id + "\n")
+
+  if possibly_removed:
+    deleted_file = f"playboard_ids_{channel_id}_deleted.txt"
+    with open(deleted_file, "w") as f:
+      for _id in possibly_removed:
+        f.write(_id + "\n")
+
+
+if __name__ == "__main__":
+  main()
