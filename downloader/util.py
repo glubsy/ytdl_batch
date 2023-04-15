dd92bc889f2e6be4626766d63813836ff52a6cc9
glubsy <glubsy@users.noreply.github.com>
2023-04-15 02:47:09 +0200

    Abort if downloader program was not found

--- downloader/util.py
+++ downloader/util.py
@@ -2,6 +2,7 @@ from typing import Optional
 from subprocess import DEVNULL, run
 from pathlib import Path
 from os.path import expanduser
+import logging
 
 
 def find_program(default_proc: str, path: Optional[str]) -> Optional[str]:
@@ -10,21 +11,19 @@ def find_program(default_proc: str, path: Optional[str]) -> Optional[str]:
   at *path* and return if found, otherwise return None.
   """
   if not path:
-    proc = run(
+    which = run(
       ['which', default_proc], check=False,
       capture_output=False, stdout=DEVNULL, stderr=DEVNULL
     )
-    if not proc.returncode == 0:
-      print(
-        f"`which {default_proc}` returned exit code {proc.returncode}, "
-        "and did not find it.")
-      return None 
+    if not which.returncode == 0:
+      raise Exception(
+        f"`which {default_proc}` returned exit code {which.returncode}, "
+        "and did not find the specified program.")
     return default_proc
 
   ppath = Path(expanduser(path))
   if ppath.exists():
-    print(f"Using process specified in ENV: \"{ppath}\".")
+    logging.info(f"Using process specified in ENV: \"{ppath}\".")
     return str(ppath)
-  else:
-    print(f"\"{ppath}\" does not exist.")
-  return None
+
+  raise Exception(f"\"{ppath}\" does not exist.")
